4df0069064ca59b4f4f48468edc8068de98f92cc
Ryan Leckey <leckey.ryan@gmail.com>
2018-08-14 22:30:20 -0700

    Configure bindings a bit

--- zsh/bindings.zsh
+++ zsh/bindings.zsh
@@ -1,3 +1,50 @@
 
-# Emacs style line editing
-bindkey -v
+# Create a zkbd-compatible key hash (using terminfo)
+typeset -A key
+
+key[Home]=${terminfo[khome]}
+key[End]=${terminfo[kend]}
+key[Insert]=${terminfo[kich1]}
+key[Delete]=${terminfo[kdch1]}
+key[Up]=${terminfo[kcuu1]}
+key[Down]=${terminfo[kcud1]}
+key[Left]=${terminfo[kcub1]}
+key[Right]=${terminfo[kcuf1]}
+key[PageUp]=${terminfo[kpp]}
+key[PageDown]=${terminfo[knp]}
+
+# Bind simple keys using terminfo (if present)
+[[ -n "${key[Home]}"    ]]  && bindkey  "${key[Home]}"    beginning-of-line
+[[ -n "${key[End]}"     ]]  && bindkey  "${key[End]}"     end-of-line
+[[ -n "${key[Insert]}"  ]]  && bindkey  "${key[Insert]}"  overwrite-mode
+[[ -n "${key[Delete]}"  ]]  && bindkey  "${key[Delete]}"  delete-char
+[[ -n "${key[Up]}"      ]]  && bindkey  "${key[Up]}"      history-beginning-search-backward
+[[ -n "${key[Down]}"    ]]  && bindkey  "${key[Down]}"    history-beginning-search-forward
+[[ -n "${key[Left]}"    ]]  && bindkey  "${key[Left]}"    backward-char
+[[ -n "${key[Right]}"   ]]  && bindkey  "${key[Right]}"   forward-char
+
+# Bind key combinations
+# TODO: Look into if its possible to use terminfo here
+bindkey "^[^[[D" backward-word
+bindkey "^[^[[C" forward-word
+
+# Force emacs-style line editing
+bindkey -e
+
+# Finally, make sure the terminal is in application mode, when zle is
+# active. Only then are the values from $terminfo valid.
+
+function zle-line-init () {
+	if (( ${+terminfo[smkx]} )); then
+		echoti smkx
+	fi
+}
+
+function zle-line-finish () {
+	if (( ${+terminfo[rmkx]} )); then
+		echoti rmkx
+	fi
+}
+
+zle -N zle-line-init
+zle -N zle-line-finish
