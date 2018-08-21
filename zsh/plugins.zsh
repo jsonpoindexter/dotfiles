4309b770bacebf34e5b530f0dc68fbd68d331f3c
Ryan Leckey <leckey.ryan@gmail.com>
2018-08-21 10:08:09 -0700

    Add history-search and autosuggestions

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -7,6 +7,23 @@
 source ~/.dotfiles/enhancd/init.sh
 
 #
+# zsh-autosuggestions
+# https://github.com/zsh-users/zsh-autosuggestions
+#
+
+ZSH_AUTOSUGGEST_USE_ASYNC=1
+ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=25      # disables suggestion for large buffers
+
+source ~/.dotfiles/zsh-autosuggestions/zsh-autosuggestions.zsh
+
+#
+# history-search-multi-word
+# https://github.com/zdharma/history-search-multi-word
+#
+
+source ~/.dotfiles/history-search-multi-word/history-search-multi-word.plugin.zsh
+
+#
 # spaceship prompt
 # https://github.com/denysdovhan/spaceship-prompt
 #
