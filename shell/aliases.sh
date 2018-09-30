fe3bd224cb8a407d6c6a6545b91f0b6cb30e173b
Ryan Leckey <ryan@launchbadge.com>
2018-09-29 22:14:00 -0700

    rg -> ripgrep and remove ncdu (annoying to use as a replacement for du)

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,7 +1,7 @@
 
 # ripgrep -> grep
 # https://github.com/BurntSushi/ripgrep
-alias grep='ripgrep'
+alias grep='rg'
 
 # exa -> ls
 # https://github.com/ogham/exa
@@ -20,10 +20,6 @@ alias cat='bat'
 # prettyping -> ping
 # https://github.com/denilsonsa/prettyping
 alias ping='prettyping --nolegend'
-
-# ncdu -> du
-# https://github.com/rofl0r/ncdu
-alias du="ncdu --color dark -rr -x --exclude .git --exclude node_modules"
 
 # glances -> top
 # https://nicolargo.github.io/glances/
